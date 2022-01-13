cc17ba948d0e582556f5a818956d3597d2f5591a
fawda123 <mbafs2012@gmail.com>
2022-01-13 16:20:52 -0500

    extract relevant code for running app from shinyBatch, back to tbeprsparrow in separate repo

--- app.R
+++ app.R
@@ -1,2 +1,33 @@
-devtools::load_all('RSPARROW_Master')
-runBatchShiny("results/TampaTP_test4")
+devtools::load_all('../tbepRSParrow')
+
+path_PastResults <- "results/TampaTP_test4"
+path_PastResults<-paste0(path_PastResults,"/maps/shinyArgs")
+load(path_PastResults)
+
+unPackList(lists = list(shinyArgs = shinyArgs),
+           parentObj = list(NA)) 
+unPackList(lists = list(file.output.list = file.output.list),
+           parentObj = list(NA)) 
+
+#trigger shiny
+shiny::runApp(shinyMap2(
+  #stream/catchment
+  file.output.list,map_uncertainties,BootUncertainties,
+  data_names,mapping.input.list,
+  #predict.list,
+  subdata,SelParmValues,
+  #site attr
+  sitedata,
+  #scenarios
+  estimate.list,estimate.input.list,
+  ConcFactor,DataMatrix.list,dlvdsgn,
+  reach_decay_specification,reservoir_decay_specification,
+  scenario.input.list,if_predict,
+  #scenarios out
+  add_vars,
+  #batchError
+  batch_mode,
+  RSPARROW_errorOption))
+
+
+
